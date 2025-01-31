517172d0380819af16fa61a8b22629cef5ce5551
parmjit kanth <parmjitk@gmail.com>
2025-01-30 22:52:53 -0500

    Update search.tsx

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -10,7 +10,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const { replace } = useRouter();
  
 
-  function handleSearch(term: string) {
+  //function handleSearch(term: string) {
     const handleSearch = useDebouncedCallback((term) => {
       console.log(`Searching... ${term}`);
      
@@ -23,7 +23,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
       }
       replace(`${pathname}?${params.toString()}`);
     }, 300);
-  }
+  //}
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
